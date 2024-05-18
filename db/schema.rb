f5692096ea64de7c40773a17de0242ab2d10e746
Gabriela Cardoso <157655310+gabicards@users.noreply.github.com>
2024-05-18 09:56:50 +0200

    Header

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_14_134332) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_16_172348) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pins", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
